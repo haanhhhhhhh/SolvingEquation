3e423b4e85f26d6962ea3a55823c81db487f90b7
naicon <naicon@localhost>
2023-09-29 21:20:05 +0700

    SolvingEquaton

---
+++ src/newpackage/Main.java
@@ -0,0 +1,10 @@
+
+package newpackage;
+
+import controller.EquationManagement;
+
+public class Main {
+    public static void main(String[] args) {
+        new EquationManagement().run();
+    }
+}
